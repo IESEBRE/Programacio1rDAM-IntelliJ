3bfb2a210143af005d3a9de4e472539e07d52160
Santi Sabate <ssabate@iesebre.com>
2023-05-10 21:02:07 +0200

    Commit inicial

---
+++ MVCGUI_PlantillaMaven/src/main/java/com/iesebre/mvc/view/View.java
@@ -0,0 +1,20 @@
+package com.iesebre.mvc.view;
+
+import javax.swing.*;
+
+public class View extends JFrame {
+    private JPanel panel;
+    private JButton button1;
+
+    public View(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+
+    }
+
+    public JButton getButton1() {
+        return button1;
+    }
+}
